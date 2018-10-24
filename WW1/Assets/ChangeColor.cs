9505d8a73a26a5e57c61bb3718a036573680b0a2
rfgoodni <rfgoodni@ncsu.edu>
2018-10-24 15:01:17 -0400

    Rooms included, a first pass at teleporting and holding objects

---
+++ WW1/Assets/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+    private MeshRenderer render;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cube1")
+        {
+            transform.Translate(1000, 1, 10, Space.World);
+            transform.Rotate(0, 180, 0);
+            render = col.gameObject.GetComponent<MeshRenderer>();
+            render.material.color = Color.green;
+        } else if (col.gameObject.name == "Cube2")
+        {
+            transform.Translate(-1000, 1, 10, Space.World);
+            transform.Rotate(0, 180, 0);
+            render = col.gameObject.GetComponent<MeshRenderer>();
+            render.material.color = Color.red;
+        }
+    }
+}
---
+++ WW1/Assets/PickupObject.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    GameObject mainCamera;
+    bool carrying;
+    GameObject carriedObject;
+    public float distance;
+    public float smooth;
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GameObject.Find("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (carrying)
+        {
+            Carry(carriedObject);
+            CheckDrop();
+        }
+        else
+        {
+            Pickup();
+        }
+    }
+
+    void Carry(GameObject o)
+    {
+        o.transform.position = Vector3.Lerp(o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+    }
+
+    void Pickup()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Pickupable p = hit.collider.GetComponent<Pickupable>();
+                if (p != null)
+                {
+                    carrying = true;
+                    carriedObject = p.gameObject;
+                    gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+        }
+    }
+
+    void CheckDrop()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            DropObject();
+        }
+    }
+
+    void DropObject()
+    {
+        carrying = false;
+        carriedObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject = null;
+    }
+}
